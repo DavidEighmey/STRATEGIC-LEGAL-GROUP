4b15d6fe22f270f857decd6b4677b8d976e1c950
David Eighmey <deighmey11@gmail.com>
2017-02-16 18:33:23 -0800

    fixed the landing page

--- src/Landing.js
+++ src/Landing.js
@@ -4,13 +4,17 @@ import './Landing.css';
 class Landing extends Component{
   render(){
   	return (
-  		<div className="landing-background">
-	  		<h1 className="site-header">Strategic Legal Group</h1>
-	  		<h3 className="site-description">
-	  			We provide quality legal services and expert representation for clients in the Central Valley and throughout California. When it comes to matters of the law, what you don’t know can often hurt you. We help you from the beginning, giving you the personalized attention you deserve.
-	  		</h3>
-	  		<button className="btn btn-default btn-lg"> About Us </button>
-  		</div>
+  		<div className="jumbotron">
+  			<div className="container">
+	  			<div className="landing-background">
+		  			<h1 className="site-header page-header">Strategic Legal Group</h1>
+		  			<h3 className="site-description">
+		  				We provide quality legal services and expert representation for clients in the Central Valley and throughout California. When it comes to matters of the law, what you don’t know can often hurt you. We help you from the beginning, giving you the personalized attention you deserve.
+		  			</h3>
+		  			<button className="btn btn-default btn-lg"> About Us </button>
+	  			</div>
+	  		</div>
+			</div>
 		);
   }
 }
